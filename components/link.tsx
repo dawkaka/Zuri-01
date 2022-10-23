c62bbc3983deac73c387566bf1b8af36185b4a4b
dawkaka <yousiph77@gmail.com>
2022-10-23 19:25:14 +0000

    Update link titles

--- components/link.tsx
+++ components/link.tsx
@@ -3,27 +3,27 @@ import "../styles/link.css"
 
 const links = [
     {
-        title: "Zuri Team",
+        title: "Meet The Zuri Team",
         url: "https://training.zuri.team/",
         id: "btn__zuri"
     },
     {
-        title: "Zuri Books",
+        title: "Books By Zuri Team",
         url: "http://books.zuri.team/",
         id: "books"
     },
     {
-        title: "Python books",
+        title: "The Best Python books",
         url: "https://books.zuri.team/python-for-beginners?ref_id=devGrammer",
         id: "book__python"
     },
     {
-        title: "Background Check for Coders",
+        title: "Know your coders",
         url: "https://background.zuri.team",
         id: "pitch"
     },
     {
-        title: "Design Books",
+        title: "The Best Design Books",
         url: "https://books.zuri.team/design-rules",
         id: "book__design"
     }
