397b0779c72c22779f8d1a490e1779d4e200042d
dawkaka <yousiph77@gmail.com>
2022-11-03 17:24:46 +0000

    feat: add contact page header

---
+++ src/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "../styles/contact.css"
+
+export default function Contact() {
+    return (
+        <div className="contact-container">
+            <main className="contact-main">
+                <div className="contact-content">
+                    <div className="contact-header-container">
+                        <h3 className="text-l" style={{ fontSize: "36px" }}>Contact Me</h3>
+                        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+                    </div>
+                </div>
+
+            </main>
+
+        </div>
+    )
+}
